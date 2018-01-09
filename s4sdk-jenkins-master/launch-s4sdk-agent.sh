c550982ae0315d97d0678e193b32a5d098d1242f
Daniel Kurzynski <daniel.kurzynski@sap.com>
2018-01-09 16:45:49 +0100

    Add Jenkins agent
    * Rename slave to agent
    
    * correct typo
    
    * correct the device name /dev/null
    
    * Remove broken formatting
    
    * Set executable bit for shell script

---
+++ s4sdk-jenkins-master/launch-s4sdk-agent.sh
@@ -0,0 +1,66 @@
+#! /bin/bash
+
+function log_error()
+{
+    echo -e "\033[1;31m[Error]\033[0m $1" >&2
+}
+
+if [ $# -lt 2 ]; then
+  echo "Please provide the following parameters: $0 USER HOST [DOCKER_IMAGE]"
+  exit 1
+fi
+
+cd ~
+
+SCP_COMMAND="scp -B -o StrictHostKeyChecking=no"
+SSH_COMMAND="ssh -x"
+
+SSH_USER=$1
+SSH_HOST=$2
+REMOTE_DIR=s4sdk_jenkins_agent
+
+if [ -n "$3" ]; then
+  DOCKER_IMAGE=$3
+else
+  DOCKER_IMAGE='s4sdk/jenkins-agent:latest'
+fi
+
+echo "Download slave.jar"
+wget http://localhost:8080/jnlpJars/slave.jar
+
+echo "Create directory on agent: $REMOTE_DIR"
+$SSH_COMMAND $SSH_USER@$SSH_HOST "mkdir -p $REMOTE_DIR" </dev/null
+
+echo "Copy salve.jar to remote host"
+$SCP_COMMAND slave.jar $SSH_USER@$SSH_HOST:$REMOTE_DIR/slave.jar
+
+rm slave.jar
+
+echo "Get Docker Group ID"
+
+DOCKER_GID=$($SSH_COMMAND $SSH_USER@$SSH_HOST "grep ^docker /etc/group|cut -d: -f3" </dev/null)
+
+if [ -z "${DOCKER_GID}" ]; then
+        log_error "Failed to determine docker group id."
+        exit 1
+fi
+echo "Using Docker GID: $DOCKER_GID"
+
+echo "Update Docker Image"
+$SSH_COMMAND $SSH_USER@$SSH_HOST "docker pull $DOCKER_IMAGE" </dev/null >/dev/null
+
+echo "Start agent"
+
+if [ -n "$http_proxy" ]; then
+  PROXY_ENV=" -e http_proxy=$http_proxy"
+fi
+
+if [ -n "$https_proxy" ]; then
+  PROXY_ENV="$PROXY_ENV -e https_proxy=$https_proxy"
+fi
+
+if [ -n "$no_proxy" ]; then
+  PROXY_ENV="$PROXY_ENV -e no_proxy=$no_proxy"
+fi
+
+$SSH_COMMAND $SSH_USER@$SSH_HOST docker run -i --rm -u 1000:$DOCKER_GID -v \`pwd\`/$REMOTE_DIR:/$REMOTE_DIR -v /var/run/docker.sock:/var/run/docker.sock $PROXY_ENV $DOCKER_IMAGE java -jar /$REMOTE_DIR/slave.jar
