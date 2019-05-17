4db07a3209cf4be05ba869da886c30176c4bcfd6
Florian Geckeler <f.geckeler@sap.com>
2019-05-17 12:45:31 +0200

    Init nexus repos without caching of negative respones from the proxied repos

--- s4sdk-cxserver-companion/files/nexus-init-repos.groovy
+++ s4sdk-cxserver-companion/files/nexus-init-repos.groovy
@@ -1,19 +1,28 @@
+import org.sonatype.nexus.blobstore.api.BlobStoreManager
+import org.sonatype.nexus.repository.config.Configuration
+import org.sonatype.nexus.repository.maven.LayoutPolicy
+import org.sonatype.nexus.repository.maven.VersionPolicy
+
+
 synchronized (this) {
-    def result = [created: [], deleted: []]
+    Map result = [created: [], deleted: []]
     def repoManager = repository.repositoryManager
-    def existingRepoNames = repoManager.browse().collect { repo -> repo.name }
-    def mvnProxyName = 'mvn-proxy'
+    List existingRepoNames = repoManager.browse().collect { repo -> repo.name }
+    String mvnProxyName = 'mvn-proxy'
     if (!existingRepoNames.contains(mvnProxyName)) {
-        repository.createMavenProxy(mvnProxyName, '<%= mvn_repository_url %>')
+        Configuration config = this.createProxyConfigWithoutNegativeCache(mvnProxyName, 'maven2-proxy', '<%= mvn_repository_url %>')
+        config.attributes.maven = [versionPolicy: VersionPolicy.RELEASE, layoutPolicy: LayoutPolicy.STRICT]
+        repository.createRepository(config)
         result.created.add(mvnProxyName)
     }
-    def npmProxyName = 'npm-proxy'
+    String npmProxyName = 'npm-proxy'
     if (!existingRepoNames.contains(npmProxyName)) {
-        repository.createNpmProxy(npmProxyName, '<%= npm_registry_url %>')
+        Configuration config = this.createProxyConfigWithoutNegativeCache(npmProxyName, 'npm-proxy', '<%= npm_registry_url %>')
+        repository.createRepository(config)
         result.created.add(npmProxyName)
     }
-    def proxyRepos = [npmProxyName, mvnProxyName]
-    def toDelete = existingRepoNames.findAll { !proxyRepos.contains(it) }
+    List proxyRepos = [npmProxyName, mvnProxyName]
+    List toDelete = existingRepoNames.findAll { !proxyRepos.contains(it) }
     for (def repoName : toDelete) {
         repoManager.delete(repoName)
         result.deleted.add(repoName)
@@ -59,7 +68,7 @@ synchronized (this) {
     return result
 }
 
-def extractCredentials(URL proxyURL) {
+Map extractCredentials(URL proxyURL) {
     def userInfo = proxyURL.getUserInfo()
     if(!userInfo) {
         return null
@@ -71,4 +80,34 @@ def extractCredentials(URL proxyURL) {
     }
 
     return [ username: URLDecoder.decode(splitted[0], "UTF-8"), password: URLDecoder.decode(splitted[1], "UTF-8") ]
+}
+
+private Configuration createProxyConfigWithoutNegativeCache(String name, String recipeName, String remoteUrl) {
+    Configuration config = new Configuration(
+        repositoryName: name,
+        recipeName: recipeName,
+        online: true,
+        attributes: [
+            httpclient: [
+                connection: [
+                    blocked: false,
+                    autoBlock: true
+                ]
+            ],
+            proxy: [
+                remoteUrl: remoteUrl,
+                // ToDo: consider setting those 2 values to -1. See https://support.sonatype.com/hc/en-us/articles/115010182627-Understanding-Caching-Configuration
+                contentMaxAge: 1440,
+                metadataMaxAge: 1440
+            ],
+            negativeCache: [
+                enabled: false
+            ],
+            storage: [
+                blobStoreName: BlobStoreManager.DEFAULT_BLOBSTORE_NAME,
+                strictContentTypeValidation: true
+            ]
+        ]
+    )
+  return config
 }
